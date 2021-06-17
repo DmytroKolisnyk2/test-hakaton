ca18e5c39f43423f13dfd59c86bfdb665b3e9c73
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-06-17 08:34:56 +0300

    add saving func

--- js/function.js
+++ js/function.js
@@ -1,7 +1,6 @@
 const string = `<div class="card">
-<input type="text" placeholder="Your title..." class="card__headline">
- <!-- <h3 class="card__headline">Description</h3> -->
- <textarea class="card__textarea" name="text" cols="30" rows="5" placeholder="Type your text here..."></textarea>
+<input type="text" placeholder="Your title..." class="card__headline save-text">
+ <textarea class="card__textarea save-text" name="text" cols="30" rows="5" placeholder="Type your text here..."></textarea>
  <ul class="card__controls-wrapper">
     <li class="card__controls-item"><button class="card__btn card__btn--arrow">&#60;</button></li>
     <li class="card__controls-item"><button class="card__btn card__btn--trash"></button></li>
@@ -14,6 +13,11 @@ export const createCardFn = (event) => {
    document.querySelectorAll(".card__btn--trash").forEach((btn) => {
       btn.addEventListener("click", removeCard);
    });
+   document.querySelectorAll(".card__btn--trash").forEach((btn) => {
+      btn.addEventListener("click", removeCard);
+   });
+  document.querySelectorAll(".save-text").forEach((input) => input.addEventListener("input", (event) => (event.target.dataset.text = event.target.value)));
+  
 };
 
 export const removeCard = (event) => {
@@ -26,7 +30,7 @@ export const addColumn = () => {
    const columnString = `
 <div class="column">
 		   <div class="column__wrapper">
-			<input type="text" placeholder="Title of column..." value="" class="column__title"></input>
+			<input type="text" placeholder="Title of column..." value="" class="column__title save-text"></input>
 			  <div class="column__wrapper-cards" data-index="${wrapper.children.length + 1}">
 				 <div class="column__plus-wrapper">
 					<p class="column__wrapper--plus">+</p>
@@ -39,10 +43,11 @@ export const addColumn = () => {
    document.querySelectorAll(".column__wrapper--plus").forEach((btn) => {
       btn.addEventListener("click", createCardFn);
    });
+   document.querySelectorAll(".save-text").forEach((input) => input.addEventListener("input", (event) => (event.target.dataset.text = event.target.value)));
 };
 export const removeColumn = event;
 
 export const saveChanges = () => {
   const saveString = document.querySelector(".columns").innerHTML;
-  window.localStorage.setItem("progress", saveString);
-}
+   window.localStorage.setItem("progress", saveString);
+};
--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ import { addColumn } from "./function.js";
 import { saveChanges } from "./function.js";
 
 setInterval(saveChanges, 1000);
+// document.querySelector(".columns").children.forEach(child => child.remove());
 document.querySelector(".columns").innerHTML =
    window.localStorage.getItem("progress") ||
-   `<section class="columns">
-               <div class="column">
+   `<div class="column">
 		   <div class="column__wrapper">
-			  <input type="text" placeholder="Title of column..." value="To do" class="column__title"></input>
+			  <input type="text" placeholder="Title of column..." value="To do" class="column__title save-text"></input>
 			  <div class="column__wrapper-cards" data-index="1">
 				 <div class="column__plus-wrapper">
 					<p class="column__wrapper--plus">+</p>
@@ -19,7 +19,7 @@ document.querySelector(".columns").innerHTML =
 		</div>
 		<div class="column">
 		   <div class="column__wrapper">
-			<input type="text" placeholder="Title of column..." value="In progress" class="column__title"></input>
+			<input type="text" placeholder="Title of column..." value="In progress" class="column__title save-text"></input>
 			  <div class="column__wrapper-cards" data-index="2">
 				 <div class="column__plus-wrapper">
 					<p class="column__wrapper--plus">+</p>
@@ -29,17 +29,20 @@ document.querySelector(".columns").innerHTML =
 		</div>
 		<div class="column">
 		   <div class="column__wrapper">
-			<input type="text" placeholder="Title of column..." value="Done" class="column__title"></input>
+			<input type="text" placeholder="Title of column..." value="Done" class="column__title save-text"></input>
 			  <div class="column__wrapper-cards" data-index="3">
 				 <div class="column__plus-wrapper">
 					<p class="column__wrapper--plus">+</p>
 				 </div>
 			  </div>
 		   </div>
-		</div>
-            </section>
->`;
+		</div>`;
 document.querySelectorAll(".column__wrapper--plus").forEach((btn) => {
    btn.addEventListener("click", createCardFn);
 });
 document.querySelector(".column__plus-wrapper-right").addEventListener("click", addColumn);
+document.querySelectorAll(".save-text").forEach((input) => input.addEventListener("input", (event) => (event.target.dataset.text = event.target.value)));
+document.querySelectorAll(".save-text").forEach((input) => {
+   if (input.dataset.text === undefined) return;
+   input.value = input.dataset.text;
+});
