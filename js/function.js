31ec1e890ba964820d559d767c5636812cb0e0fc
MaxPopsuy <maim09p@gmail.com>
2021-06-17 08:38:21 +0300

    moveCard fixed

--- js/function.js
+++ js/function.js
@@ -3,9 +3,9 @@ const string = `<div class="card">
  <!-- <h3 class="card__headline">Description</h3> -->
  <textarea class="card__textarea" name="text" cols="30" rows="5" placeholder="Type your text here..."></textarea>
  <ul class="card__controls-wrapper">
-    <li class="card__controls-item"><button class="card__btn card__btn--arrow" data-value='left'>&#60;</button></li>
+    <li class="card__controls-item"><button class="card__btn card__btn--arrow left" data-value='left'>&#60;</button></li>
     <li class="card__controls-item"><button class="card__btn card__btn--trash"></button></li>
-    <li class="card__controls-item"><button class="card__btn card__btn--arrow data-value='right'">&#62;</button></li>
+    <li class="card__controls-item"><button class="card__btn card__btn--arrow right data-value='right'">&#62;</button></li>
  </ul>
 </div>`;
 let index;
@@ -13,26 +13,28 @@ const moveCardRightFn = (event) => {
   index =
     +event.currentTarget.parentNode.parentNode.parentNode.parentNode.dataset
       .index + 1;
-  if (index > document.querySelector(".columns").children.length) index = 1;
+  if (index === 1) index = document.querySelector(".columns").children.length;
   document
     .querySelector(`.column__wrapper-cards--${index}`)
     .appendChild(document.querySelector(".card").cloneNode(true));
   removeCard(event);
-  document.querySelectorAll(".card__btn--trash").forEach((btn) => {
-    btn.addEventListener("click", removeCard);
-  });
   document.querySelectorAll(".card__btn--arrow").forEach((elem) => {
-    if (elem.dataset.value === "right") {
-      document
-        .querySelectorAll(".card__btn--arrow")[1]
-        .addEventListener("click", moveCardRightFn);
-    }
-    if (elem.dataset.value === "left") {
-      document
-        .querySelectorAll(".card__btn--arrow")[0]
-        .addEventListener("click", moveCardLeftFn);
-    }
+    document.querySelectorAll(".left").forEach((elem) => {
+      elem.addEventListener("click", moveCardLeftFn);
+    });
+    document.querySelectorAll(".card__btn--trash").forEach((btn) => {
+      btn.addEventListener("click", removeCard);
+    });
+    document.querySelectorAll(".right").forEach((elem) => {
+      elem.addEventListener("click", moveCardRightFn);
+    });
   });
+    // document
+    //   .querySelectorAll(".card__btn--arrow")[1]
+    //   .addEventListener("click", moveCardRightFn);
+    // document
+    //   .querySelectorAll(".card__btn--arrow")[0]
+    //   .addEventListener("click", moveCardLeftFn);
 };
 
 const moveCardLeftFn = (event) => {
@@ -44,21 +46,18 @@ const moveCardLeftFn = (event) => {
     .querySelector(`.column__wrapper-cards--${index}`)
     .appendChild(document.querySelector(".card").cloneNode(true));
   removeCard(event);
-  document.querySelectorAll(".card__btn--trash").forEach((btn) => {
-    btn.addEventListener("click", removeCard);
-  });
   document.querySelectorAll(".card__btn--arrow").forEach((elem) => {
-    if (elem.dataset.value === "right") {
-      document
-        .querySelectorAll(".card__btn--arrow")[1]
-        .addEventListener("click", moveCardRightFn);
-    }
-    if (elem.dataset.value === "left") {
-      document
-        .querySelectorAll(".card__btn--arrow")[0]
-        .addEventListener("click", moveCardLeftFn);
-    }
+    document.querySelectorAll(".left").forEach((elem) => {
+      elem.addEventListener("click", moveCardLeftFn);
+    });
+    document.querySelectorAll(".card__btn--trash").forEach((btn) => {
+      btn.addEventListener("click", removeCard);
+    });
+    document.querySelectorAll(".right").forEach((elem) => {
+      elem.addEventListener("click", moveCardRightFn);
+    });
   });
+
 };
 
 export const createCardFn = (event) => {
@@ -67,16 +66,15 @@ export const createCardFn = (event) => {
     btn.addEventListener("click", removeCard);
   });
   document.querySelectorAll(".card__btn--arrow").forEach((elem) => {
-    if (elem.dataset.value === "right") {
-      document
-        .querySelectorAll(".card__btn--arrow")[1]
-        .addEventListener("click", moveCardRightFn);
-    }
-    if (elem.dataset.value ==="left") {
-      document
-        .querySelectorAll(".card__btn--arrow")[0]
-        .addEventListener("click", moveCardLeftFn);
-    }
+    document.querySelectorAll(".left").forEach((elem) => {
+      elem.addEventListener("click", moveCardLeftFn);
+    });
+    document.querySelectorAll(".card__btn--trash").forEach((btn) => {
+      btn.addEventListener("click", removeCard);
+    });
+    document.querySelectorAll(".right").forEach((elem) => {
+      elem.addEventListener("click", moveCardRightFn);
+    });
   });
 };
 
@@ -86,3 +84,4 @@ export const removeCard = (event) => {
   );
   event.currentTarget.parentNode.parentNode.parentNode.remove();
 };
+
